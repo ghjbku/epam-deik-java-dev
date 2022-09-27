8564c42c8566e11469bf8d9609f4e02df8d2d19c
Balázs Tóth <gawnoris@gmail.com>
2022-09-27 10:22:24 +0200

    Update Application.java
    added spring boot run

--- final/ticket-service/src/main/java/com/epam/training/ticketservice/Application.java
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/Application.java
@@ -1,6 +1,14 @@
 package com.epam.training.ticketservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
-    }
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
+
 }
+
