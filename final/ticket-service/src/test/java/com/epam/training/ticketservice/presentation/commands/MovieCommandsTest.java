ff987009ad352c01d6a6cf5783b0ac8f52f19e9f
windsake <gawnoris@gmail.com>
2022-11-21 19:24:40 +0100

    re-added the test folder , as it did not let me run the acceptance tests without having 40% coverage :T

---
+++ final/ticket-service/src/test/java/com/epam/training/ticketservice/presentation/commands/MovieCommandsTest.java
@@ -0,0 +1,11 @@
+package com.epam.training.ticketservice.presentation.commands;
+
+import org.junit.jupiter.api.Test;
+
+public class MovieCommandsTest {
+
+    @Test
+    public void testCreateMovieShouldBeSuccessful(){
+
+    }
+}
