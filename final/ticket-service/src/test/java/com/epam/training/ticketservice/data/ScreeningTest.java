1f86c23bd0aab105452813b94e435fb556583d0c
windsake <gawnoris@gmail.com>
2022-12-02 14:12:10 +0100

    futher testing of the Screening object

--- final/ticket-service/src/test/java/com/epam/training/ticketservice/data/ScreeningTest.java
+++ final/ticket-service/src/test/java/com/epam/training/ticketservice/data/ScreeningTest.java
@@ -2,14 +2,17 @@ package com.epam.training.ticketservice.data;
 
 import com.epam.training.ticketservice.data.screenings.ScreeningService;
 import com.epam.training.ticketservice.data.screenings.ScreeningServiceImpl;
+import com.epam.training.ticketservice.data.screenings.model.ScreeningDto;
 import com.epam.training.ticketservice.data.screenings.persistence.entity.Screening;
 import com.epam.training.ticketservice.data.screenings.persistence.repository.ScreeningRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -66,7 +69,57 @@ public class ScreeningTest {
                 .findByMovieMovieNameRoomRoomNameAndScreeningDate(testScreening.getMovieName(),
                         testScreening.getRoomName(), testScreening.getScreeningDate());
         Mockito.verify(screeningRepository).delete(testScreening);
+    }
 
+    @Test
+    public void testScreeningListAllShouldBeSuccessful() {
+        //given
+        Date screeningDate = null, screeningDate2 = null;
+        try {
+            screeningDate = sf.parse("1997-02-07 08:21");
+            screeningDate2 = sf.parse("1997-02-08 08:21");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Screening testScreening = new Screening(null, "test", "testRoom", screeningDate);
+        List<Screening> testList = List.of(testScreening,
+                new Screening(null, "test2", "testRoom", screeningDate2));
+
+        //when
+        when(screeningRepository.findAll()).thenReturn(testList);
+
+        Optional<List<ScreeningDto>> resultList=underTest.listAll();
+
+        //then
+        Mockito.verify(screeningRepository).findAll();
+        Assertions.assertTrue(resultList.isPresent());
+        Assertions.assertEquals(2, resultList.get().size());
+        Assertions.assertEquals(resultList.get().get(0).getMovieName(),testScreening.getMovieName());
+        Assertions.assertEquals(resultList.get().get(1).getMovieName(),testList.get(1).getMovieName());
+    }
+
+    @Test
+    public void testScreeningListAllShouldNotBeSuccessful() {
+        //given
+        Date screeningDate = null, screeningDate2 = null;
+        try {
+            screeningDate = sf.parse("1997-02-07 08:21");
+            screeningDate2 = sf.parse("1997-02-08 08:21");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Screening testScreening = new Screening(null, "test", "testRoom", screeningDate);
+        List<Screening> testList = List.of(testScreening,
+                new Screening(null, "test2", "testRoom", screeningDate2));
+
+        //when
+        when(screeningRepository.findAll()).thenReturn(List.of());
+
+        Optional<List<ScreeningDto>> resultList=underTest.listAll();
+
+        //then
+        Mockito.verify(screeningRepository).findAll();
+        Assertions.assertTrue(resultList.isEmpty());
     }
 
 
